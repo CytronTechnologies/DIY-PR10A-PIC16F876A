eb31df19fed1e0bd8169d30d4ac8d1c6ebe14f3b
CytronTrainee <cytontrainee@gmail.com>
2015-07-23 10:58:08 +0800

    DIY PR10A
    Sample source code for DIY PR10A.

---
+++ PR 10A HiTech 9.80/PR10-MD10C.c
@@ -0,0 +1,97 @@
+//===================================================================================================
+//	Author				: Cytron Technologies		
+//	Project				: Controlling DC Brush Motor using MD10C
+//	Project description	: This project will use PIC microcontroller to control MD10C
+//						  further drive a brush motor with variable speed and bi-directionally. 
+//====================================================================================================
+
+
+//====================================================================================================
+//	include
+//====================================================================================================
+#include <pic.h> 
+
+//====================================================================================================
+//	configuration
+//====================================================================================================
+__CONFIG (0x3F32);
+
+//====================================================================================================
+//	Define
+//====================================================================================================
+#define		BUTTON1		RA1
+#define		BUTTON2		RA2
+#define		BUTTON3		RA3
+#define		BUTTON4		RA4
+#define		DIR			RB2
+
+//===================================================================================================
+//	Function Prototype
+//===================================================================================================
+void delay(unsigned long data);
+
+//	global variable
+unsigned char temp=50;
+
+
+//===================================================================================================
+//	main function
+//===================================================================================================
+void main(void)
+{
+	
+	ADCON1 = 0x06;					//Configure Poart A as digital I/O
+
+	TRISA = 0b11111111;				//Set pin RA1 to RA4 as input for press button switch
+	TRISB = 0b00000000;				//Set pin RB1 & RB2 as output for MD10A
+	TRISC = 0b11000000;				//Set pin RC2 as output for PWM
+	
+	CCP1CON = 0b00001100;			//PWM Mode
+
+	//PWM frequecy set as 4.88KHz
+	PR2 = 0xFF;						//PWM Period Setting
+	T2CON = 0b00000101;				//Timer2 On, prescale 4
+
+	//Motor stop
+	CCPR1L = 0;						//No PWM Duty Cycle
+	DIR = 0;
+	
+	while(1)						//Infinity Loop
+	{	
+		if(BUTTON1==0)				// Motor Brake
+		{
+			CCPR1L=0;				//No PWM Duty Cycle				
+			DIR=0;
+		}
+
+		else if(BUTTON2==0)				//Change the Rotational Direction 
+		{
+			while(BUTTON2==0)continue;  // Wait BUTTON2 to be released
+			CCPR1L=temp;			
+			DIR=!DIR;
+		}
+
+		else if(BUTTON3==0)				//Speed Increment
+		{
+			if(temp<255)temp+=1;	
+			CCPR1L=temp;
+			delay(5000);			
+		}
+
+		else if(BUTTON4==0)				//Speed Decrement
+		{
+			if(temp>0)temp-=1;
+			CCPR1L=temp;			
+			delay(5000);
+		}
+	}
+}
+
+//==============================================================================================
+//	functions
+//==============================================================================================
+void delay(unsigned long data)
+{
+	for( ;data>0;data-=1);
+}
+
